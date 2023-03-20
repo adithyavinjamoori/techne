a242e8a77b0cfdbb7111871afc42c3b923e0179c
Adithya Vin <vin.adithya@gmail.com>
2023-03-20 23:31:55 +0530

    a

--- src/assets/files/courses.ts
+++ src/assets/files/courses.ts
@@ -1,17 +1,20 @@
 export const courses = [
     {
         title:"Power BI",
+        class:"powerbi",
         subTitle:"Data Visualization | Business Intelligence",
         desc:"Power BI is a business intelligence tool developed by Microsoft that allows users to create interactive visualizations and reports from various data sources to gain insights and make informed decisions.",
         imageURL:"power-bi.svg"
     },
     {
         title:"Angular",
+        class:"angular",
         subTitle:"JavaScript framework",
         desc:"Angular is a popular open-source web application framework that was developed by Google. It is used to create dynamic, single-page web applications that are highly interactive and responsive.",
         imageURL:"Angular.png"
     },{
         title:"Power Apps",
+        class:"power-app",
         subTitle:"Custom Business Solutions",
         desc:"Power Apps is a low-code platform developed by Microsoft that allows users to build custom business applications without requiring extensive coding knowledge.",
         imageURL:"power-apps.png"
